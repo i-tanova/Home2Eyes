7fced5556aa792f742670a286f0028795c81709e
ivana.tanova <tanovait@gmail.com>
2020-07-17 09:55:58 +0300

    Fix navigation

--- app/src/main/java/com/ivanatanova/home2eye/ui/auth/AuthenticationActivity.kt
+++ app/src/main/java/com/ivanatanova/home2eye/ui/auth/AuthenticationActivity.kt
@@ -7,9 +7,9 @@ import com.ivanatanova.home2eye.R
 
 class AuthenticationActivity : AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_authentication)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
     }
 
 }
